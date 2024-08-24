f6db1b42ffd852fd3e4c6eb70caaa22964acb1aa
Vinay Thakor <vinay.th.24@gmail.com>
2024-08-24 13:40:59 +0530

    q1

---
+++ 20/digitToNumb.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int num;
+int userValue() {
+  int a;
+  cout << "Enter a number: ";
+  cin >> a;
+  return a;
+}
+void digitToNum() {
+  int n = 0;
+  cout << "\nEnter the digits as you want and when you want to terminate enter "
+          "'-1'\n\n";
+  while (n != -1) {
+    int n = userValue();
+    if (n == -1) {
+      break;
+    }
+    num = (num * 10) + n;
+    cout << "Number created so far: " << num << endl;
+  }
+}
+int main() {
+  int n;
+
+  digitToNum();
+
+  cout << "\nThe number is : " << num;
+
+  cout << "\n";
+  return 0;
+}
